2f7e183b5c6e5670e598c745f2d88de9dd69958e
YassineAzdoud <yassinelife2030@gmail.com>
2021-04-10 17:54:40 +0100

    add timeline

--- 02_Timeline/script.js
+++ 02_Timeline/script.js
@@ -1,1 +1,27 @@
-// Votre code javascript
+
+  
+ 
+    var items = document.querySelectorAll(".timeline li");
+ 
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+      
+        rect.bottom <= window.innerHeight 
+        
+      );
+    }
+  
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+  
+    // listen for events
+
+    window.addEventListener("scroll", callbackFunc);
+  
